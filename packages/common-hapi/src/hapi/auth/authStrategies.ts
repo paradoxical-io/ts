aca7c2b7156c08698607d28a31483d15943f077d
Anton Kropp <anton@getcopper.com>
2023-06-29 15:24:01 -0700

    Bundling types, allowing routes to be part of validation

--- packages/common-hapi/src/hapi/auth/authStrategies.ts
+++
@@ -1,7 +0,0 @@
-export type AuthStrategy = 'phone' | 'password' | 'google';
-
-export const authStrategies = {
-  phoneAuth: 'phone' as AuthStrategy,
-  passwordAuth: 'password' as AuthStrategy,
-  adminAuth: 'session' as AuthStrategy,
-};
