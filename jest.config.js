6a931b974aa6ec5b90ebca3ca6326b6ee6c3550c
Reid <reid.knight@hey.com>
2025-06-26 11:41:46 -0700

    Fix snapshot

--- jest.config.js
+++ jest.config.js
@@ -45,4 +45,14 @@ module.exports = {
 
   // The test environment that will be used for testing
   testEnvironment: 'node',
+
+  snapshotResolver: process.env.CI ? `${CI_DATA.root}/packages/common-test/dist/jest/snapshotResolver.js` : undefined,
+
+  transform: {
+    "^.+\\.tsx?$": ['ts-jest', {
+      // https://github.com/kulshekhar/ts-jest/issues/259#issuecomment-504088010
+      maxWorkers: 1,
+      diagnostics: false,
+    }]
+  },
 };
