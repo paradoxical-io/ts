f80a08e3b5f2fb2e390189da81ade0908c5c5dee
Anton Kropp <anton@coastapp.com>
2025-07-10 13:42:14 -0700

    Rename bottom param

--- packages/types/src/exhaustiveness.ts
+++ packages/types/src/exhaustiveness.ts
@@ -6,13 +6,13 @@ export class BottomException extends Error {}
  *
  * https://basarat.gitbooks.io/typescript/docs/types/never.html
  *
- * @param message The bottom value
+ * @param value The bottom value
  * @param onNever If the bottom is actually executed an action response that can optionally return a default value
  */
-export function bottom<T = never>(message: never, onNever?: (value: never) => T): T | never {
+export function bottom<T = never>(value: never, onNever?: (value: never) => T): T | never {
   if (!onNever) {
-    throw new BottomException(`bottom hit: ${JSON.stringify(message)}`);
+    throw new BottomException(`bottom hit: ${JSON.stringify(value)}`);
   } else {
-    return onNever(message)
+    return onNever(value)
   }
 }
