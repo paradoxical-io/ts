c858019332d75b1465f3140fe9f15cf4bebf9d9c
Anton Kropp <anton.kropp@taxbit.com>
2025-12-12 16:14:31 -0800

    Validate.js is deprecated and we dont need to pull it in

--- packages/common/src/utils/email.ts
+++
@@ -1,9 +0,0 @@
-import validate from 'validate.js';
-
-/**
- * Real email validation requires emailing the user but we can validate a good portion of email addresses
- * @param s
- */
-export function emailValid(s: string): boolean {
-  return validate({ from: s }, { from: { email: true } }) === undefined;
-}
